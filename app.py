968b0af35e52c43cd2238abd22014a4a3fed2a4f
Karthik <karthik986340@gmail.com>
2025-05-22 15:03:55 +0530

    app.py

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def handle_message(message):
     emit('chat response', response)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), allow_unsafe_werkzeug=True)
